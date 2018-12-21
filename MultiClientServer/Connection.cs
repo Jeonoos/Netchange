89ed10ad3b2b70974b03d690858538b771f5b635
Jeroen Vreugdenhil <j.vreugdenhil2@students.uu.nl>
2018-12-21 17:18:47 +0100

    Initial commit

---
+++ MultiClientServer/Connection.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace MultiClientServer
+{
+    class Connection
+    {
+        public StreamReader Read;
+        public StreamWriter Write;
+        public bool ready = false;
+        // Connection heeft 2 constructoren: deze constructor wordt gebruikt als wij CLIENT worden bij een andere SERVER
+        public Connection(int port)
+        {
+            TcpClient client = null;
+            while (client == null)
+            {
+                try
+                {
+                    client = new TcpClient("localhost", port);
+                }
+                catch { }
+            }
+            Read = new StreamReader(client.GetStream());
+            Write = new StreamWriter(client.GetStream());
+            Write.AutoFlush = true;
+            // De server kan niet zien van welke poort wij client zijn, dit moeten we apart laten weten
+            Write.WriteLine("Poort: " + Program.MijnPoort);
+
+
+
+            // Start het reader-loopje
+            new Thread(ReaderThread).Start();
+        }
+
+        // Deze constructor wordt gebruikt als wij SERVER zijn en een CLIENT maakt met ons verbinding
+        public Connection(StreamReader read, StreamWriter write)
+        {
+            Read = read; Write = write;
+
+            // Start het reader-loopje
+            new Thread(ReaderThread).Start();
+        }
+
+        // LET OP: Nadat er verbinding is gelegd, kun je vergeten wie er client/server is (en dat kun je aan het Connection-object dus ook niet zien!)
+
+        // Deze loop leest wat er binnenkomt en print dit
+        public void ReaderThread() {
+            ready = true;
+            while (Program.Initializing) { }
+            try
+            {
+                while (true) {
+                    string input = Read.ReadLine();
+                    Console.WriteLine("received: " + input);
+                    string[] message = input.Split(new char[] { ' ' }, 2);
+                    string[] args = message[1].Split();
+                    switch (message[0]) {
+                        case "mydist":
+                            lock (Program.prefBuurLock)
+                            {
+                                if (!Program.Nodes.Contains(int.Parse(args[1])))
+                                    Program.Nodes.Add(int.Parse(args[1]));
+                                if (!Program.Afstand.ContainsKey(int.Parse(args[1])))
+                                    Program.Afstand[int.Parse(args[1])] = Program.Netwerkgrootte;
+                                Program.BuurAfstand[new Tuple<int, int>(int.Parse(args[0]), int.Parse(args[1]))] = int.Parse(args[2]);
+                                Program.Recompute(int.Parse(args[1]));
+                            }
+                            break;
+                        case "B":
+                            string[] splitMessage = message[1].Split(new char[] { ' ' }, 2);
+                            if (int.Parse(splitMessage[0]) == Program.MijnPoort)
+                                Console.WriteLine(splitMessage[1]);
+                            else
+                            {
+                                Console.WriteLine(string.Format("Bericht voor {0} doorgestuurd naar {1}",splitMessage[0], Program.PrefBuur[int.Parse(splitMessage[0])]));
+                                Program.Buren[Program.PrefBuur[int.Parse(splitMessage[0])]].Write.WriteLine(input);
+                            }
+                            break;
+                        case "C":
+                            int port = int.Parse(args[0]);
+                            if (!Program.Nodes.Contains(port))
+                                Program.Nodes.Add(port);
+                            while (!Program.Buren.ContainsKey(port)) { }
+                            lock (Program.prefBuurLock)
+                            {
+                                foreach (KeyValuePair<int, int> node in Program.Afstand)
+                                {
+                                    Program.Buren[port].Write.WriteLine(Program.MyDistance(node.Key, node.Value));
+                                }
+                            }
+                            break;
+                    }
+                }
+        }
+            catch(Exception e) {
+                Console.WriteLine("thread broke: " + e.StackTrace);
+            } // Verbinding is kennelijk verbroken
+}
+    }
+}
---
+++ MultiClientServer/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiClientServer
+{
+    class Program
+    {
+        static public int MijnPoort;
+        static public int Netwerkgrootte = 20;
+        static public Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
+        static public Dictionary<int, int> Afstand = new Dictionary<int, int>();
+        static public Dictionary<int, int> PrefBuur = new Dictionary<int, int>();
+        static public Dictionary<Tuple<int, int>, int> BuurAfstand = new Dictionary<Tuple<int, int>, int>();
+        static public List<int> Nodes = new List<int>();
+        static public object prefBuurLock = new object();
+        static public bool Initializing = true;
+
+        static void Main(string[] args) {
+            if (args.Length > 0)
+                MijnPoort = int.Parse(args[0]);
+            Console.WriteLine("ik ben poort " + MijnPoort);
+
+            new Server(MijnPoort);
+            for (int i = 1; i < args.Length; i++)
+            {
+                int node = int.Parse(args[i]);
+                Nodes.Add(node);
+                Afstand[node] = Netwerkgrootte;
+                if (node > MijnPoort)
+                    Buren.Add(node, new Connection(node));
+            }
+            Nodes.Add(MijnPoort);
+            while (Buren.Count < args.Length - 1) { }
+                foreach (KeyValuePair<int, Connection> buur in Buren)
+                {
+                foreach (int node in Nodes)
+                {
+                    Console.WriteLine(string.Format("adding {0} {1}", buur.Key, node));
+                    if (!BuurAfstand.ContainsKey(new Tuple<int, int>(buur.Key, node)))
+                        BuurAfstand[new Tuple<int, int>(buur.Key, node)] = Netwerkgrootte;
+                    else
+                        Console.WriteLine("yeet");
+
+                }
+                Afstand[MijnPoort] = 0;
+                PrefBuur[MijnPoort] = MijnPoort;
+            }
+            foreach (KeyValuePair<int, Connection> buur in Buren)
+            {
+                buur.Value.Write.WriteLine(MyDistance(MijnPoort, 0));
+            }
+
+            Initializing = false;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string[] splitInput = input.Split(new char[] { ' ' }, 2);
+                switch (splitInput[0])
+                {
+                    case "R":
+                        Console.WriteLine("routing table");
+                        foreach (KeyValuePair<int, int> node in PrefBuur)
+                        {
+                            if (node.Value == MijnPoort)
+                                Console.WriteLine(string.Format("{0} {1} {2}", node.Key, Afstand[node.Key], "local"));
+                            else
+                                Console.WriteLine(string.Format("{0} {1} {2}", node.Key, Afstand[node.Key], node.Value));
+                        }
+                        break;
+                    case "B":
+                        string[] splitMessage = splitInput[1].Split(new char[] { ' ' }, 2);
+                        if (PrefBuur.ContainsKey(int.Parse(splitMessage[0])))
+                            Buren[PrefBuur[int.Parse(splitMessage[0])]].Write.WriteLine(input);
+                        else
+                            Console.WriteLine(string.Format("Poort {0} is niet bekend",splitMessage[0]));
+                        break;
+                    case "C":
+                        if (Buren.ContainsKey(int.Parse(splitInput[1])))
+                            Console.WriteLine("already connected");
+                        else
+                        {
+                            int port = int.Parse(splitInput[1]);
+                            if (!Nodes.Contains(port))
+                                Nodes.Add(port);
+                            Buren.Add(int.Parse(splitInput[1]), new Connection(int.Parse(splitInput[1])));
+                            Buren[int.Parse(splitInput[1])].Write.WriteLine(string.Format("C {0}", MijnPoort));
+                            if (!Nodes.Contains(int.Parse(splitInput[1])))
+                                Nodes.Add(int.Parse(splitInput[1]));
+                            while (!Buren[int.Parse(splitInput[1])].ready) { }
+
+                            foreach (int node in Nodes)
+                            {
+                                Buren[int.Parse(splitInput[1])].Write.WriteLine(MyDistance(node, Afstand[node]));
+                            }
+                        }
+                        break;
+                    default: break;
+                }
+            }
+        }
+        static public string MyDistance(int node, int cost) {
+            return string.Format("mydist {0} {1} {2}", MijnPoort, node, cost);
+        }
+        static public void Recompute(int port) {
+            if (port == MijnPoort)
+            {
+                BuurAfstand[new Tuple<int, int>(MijnPoort, port)] = 0;
+                PrefBuur[port] = port;
+            }
+            else
+            {
+                int oudeAfstand = Afstand[port];
+                int d = Netwerkgrootte;
+                int prefN = -1;
+                if (Buren.ContainsKey(port))
+                {
+                    Afstand[port] = 1;
+                    PrefBuur[port] = port;
+                }
+                else
+                {
+                    foreach (KeyValuePair<int, Connection> buur in Buren)
+                    {
+                        if (!BuurAfstand.ContainsKey(new Tuple<int, int>(buur.Key, port)))
+                            Console.WriteLine("bestaat niet: " + buur.Key + " naar " + port);
+                        else if (BuurAfstand[new Tuple<int, int>(buur.Key, port)] < d)
+                        {
+                            d = BuurAfstand[new Tuple<int, int>(buur.Key, port)] + 1;
+                            prefN = buur.Key;
+                        }
+                    }
+                    Console.WriteLine(string.Format("Afstand naar {0} is nu {1} via {2}", port, d, prefN));
+                    Afstand[port] = d;
+                    Console.WriteLine("oude afstand: " + oudeAfstand + ", nieuwe afstand: " + d);
+                    PrefBuur[port] = (prefN == -1) ? port : prefN;
+                }
+                if (Afstand[port] != oudeAfstand)
+                {
+                    Console.WriteLine("afstand veranderd");
+                    foreach (KeyValuePair<int, Connection> buur in Buren)
+                    {
+                        Console.WriteLine("sending to buur " + buur.Key);
+                        buur.Value.Write.WriteLine(MyDistance(port, Afstand[port]));
+                    }
+                }
+
+            }
+        }
+    }
+}
